5f4225714c083a16dfc8ccf5e2d84462fa412ed8
Daniel Boggs <daniel.boggs@gmail.com>
2020-07-16 14:03:09 -0400

    chore: add rich text styles to the prototype page

--- src/components/pages/Prototype/Prototype.jsx
+++ src/components/pages/Prototype/Prototype.jsx
@@ -6,7 +6,40 @@ const Prototype = () => {
   return (
     <Layout.Padding style={{ marginTop: "1rem", marginBottom: "2rem" }}>
       <Layout.Wrapper>
-        <Text.H1>Prototype</Text.H1>
+        <Text.Rich>
+          <Text.H1>Heading 1</Text.H1>
+          <Text.H2>Heading 2</Text.H2>
+          <Text.H3>Heading 3</Text.H3>
+          <Text.H4>Heading 4</Text.H4>
+          <Text.H5>Heading 5</Text.H5>
+          <Text.H6>Heading 6</Text.H6>
+
+          <Text.Intro>
+            Intro Enim eiusmod cupidatat irure elit veniam voluptate voluptate
+            commodo voluptate duis. Quis enim aliquip consectetur adipisicing
+            sit cillum officia ullamco. Reprehenderit ea minim ullamco sint
+            deserunt proident ipsum nisi eu ullamco elit aliqua aute dolore.
+          </Text.Intro>
+
+          <p>
+            Aute sint consequat sint aliqua. Tempor elit occaecat reprehenderit
+            ad in tempor et incididunt exercitation esse. Eiusmod et incididunt
+            labore ex do in anim sit excepteur mollit mollit Lorem. Eiusmod ut
+            do in ex officia in.
+          </p>
+          <p>
+            Aute sint consequat sint aliqua. Tempor elit occaecat reprehenderit
+            ad in tempor et incididunt exercitation esse. Eiusmod et incididunt
+            labore ex do in anim sit excepteur mollit mollit Lorem. Eiusmod ut
+            do in ex officia in.
+          </p>
+          <p>
+            Aute sint consequat sint aliqua. Tempor elit occaecat reprehenderit
+            ad in tempor et incididunt exercitation esse. Eiusmod et incididunt
+            labore ex do in anim sit excepteur mollit mollit Lorem. Eiusmod ut
+            do in ex officia in.
+          </p>
+        </Text.Rich>
       </Layout.Wrapper>
     </Layout.Padding>
   );
