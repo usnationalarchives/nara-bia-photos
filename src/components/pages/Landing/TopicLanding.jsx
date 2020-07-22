d043244acc9fda34d85b95864f19629d468011bc
Daniel Boggs <daniel.boggs@gmail.com>
2020-07-22 14:30:42 -0400

    feat: scaffolding for landing page billboards

--- src/components/pages/Landing/TopicLanding.jsx
+++ src/components/pages/Landing/TopicLanding.jsx
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 // components
 import * as Layout from "#components/shared/Layout";
-import * as Text from "#components/shared/Text";
+import Billboard from "./Billboard";
 
 // modules
 import { topics } from "#modules/constants";
 
 const TopicLanding = () => {
   return (
-    <Layout.Padding>
-      <Layout.Wrapper>
-        <Text.H1>Topics</Text.H1>
-        <ol>
-          {topics.map((topic) => (
-            <li key={topic.slug}>
-              <Link to={`/topics/${topic.slug}`}>{topic.name}</Link>
-            </li>
-          ))}
-        </ol>
-      </Layout.Wrapper>
-    </Layout.Padding>
+    <Fragment>
+      <Billboard title="Topics" description="Lorem Ipsum"></Billboard>
+
+      <Layout.Padding>
+        <Layout.Wrapper>
+          <ol>
+            {topics.map((topic) => (
+              <li key={topic.slug}>
+                <Link to={`/topics/${topic.slug}`}>{topic.name}</Link>
+              </li>
+            ))}
+          </ol>
+        </Layout.Wrapper>
+      </Layout.Padding>
+    </Fragment>
   );
 };
 
