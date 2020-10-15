1b11618c6ea7a621deefdc166fc4250850645127
Mat Brady <matbr8dy@gmail.com>
2020-10-15 14:00:26 -0400

    accessiblity: #145 adding assistive text to filters

--- src/components/shared/SelectedFilter.jsx
+++ src/components/shared/SelectedFilter.jsx
@@ -10,7 +10,7 @@ const StyledSelectedFilters = styled(Button)`
 const SelectedFilter = ({ dispatchItems, value }) => {
   return (
     <StyledSelectedFilters scheme="blue" onClick={() => dispatchItems({ type: 'remove', value: value })}>
-      {value} <SearchIcon width="11" fill="currentColor" />
+      <span className="screenreader">Filter: </span>{value} <SearchIcon width="11" fill="currentColor" />
     </StyledSelectedFilters>
   );
 };
