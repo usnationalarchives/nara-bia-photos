193cf6239f777be4151176bc18f4a829093eb6c2
Mat Brady <matbr8dy@gmail.com>
2020-11-13 08:56:20 -0500

    #131 add loading fallback

--- src/components/pages/Record/Record.jsx
+++ src/components/pages/Record/Record.jsx
@@ -22,6 +22,7 @@ import iiifImage from '#modules/iiifImage';
 
 // components
 import * as Text from '#components/shared/Text';
+import PageLoader from '#components/shared/PageLoader';
 import * as Table from '#components/shared/Table';
 import * as Layout from '#components/shared/Layout';
 import ImageViewer from './ImageViewer';
@@ -132,176 +133,179 @@ const Record = ({ ...props }) => {
     }
   }, [record]);
 
-  return (
-    <>
-      <Helmet>
-        <title>{!!record ? record.title : getIdFromPathname(location.pathname)}</title>
-        {!!record && !!record.scopeContentNote && <meta name="description" content={record.scopeContentNote} />}
-        <meta name="" content="" />
-        <meta name="twitter:title" content={!!record ? record.title : getIdFromPathname(location.pathname)} />
-        <meta name="twitter:site" content="@FIXME" />
-        {!!record && !!record.scopeContentNote && <meta name="twitter:card" content={record.scopeContentNote} />}
-        {!!record && !!record.scopeContentNote && <meta name="twitter:description" content={record.scopeContentNote} />}
-        {!!record && <meta name="twitter:image" content={iiifImage(record, '1080')} />}
-        <meta property="og:title" content={!!record ? record.title : getIdFromPathname(location.pathname)} />
-        {!!record && !!record.scopeContentNote && <meta name="og:description" content={record.scopeContentNote} />}
-        <meta property="og:site_name" content="FIXME" />
-        <meta property="og:url" content={window.location} />
-        <meta property="og:type" content="article" />
-        {!!record && <meta property="og:image" content={iiifImage(record, '1080')} />}
-      </Helmet>
-      {!!record && (
-        <>
-          <Layout.Padding style={{ marginTop: '2rem', marginBottom: '3rem' }}>
-            <Layout.Wrapper>
-              <Shave textRef={titleTextRef} maxHeight={130} options={{ character: '&nbsp; ' }}>
-                <Text.H1 ref={titleTextRef}>{record.title}</Text.H1>
-              </Shave>
-              <MetaWrapper>
-                {tribalNation && (
-                  <MetaStyled
-                    label="Tribal Nation"
-                    outine
-                    items={[
-                      {
-                        label: record.tribes,
-                        link: `/tribal-nations/${tribalNation ? tribalNation.slug : ''}`,
-                        onClick: () => {
-                          history.push(`/tribal-nations/${tribalNation ? tribalNation.slug : ''}`);
-                        },
+  if (!!record) {
+    return (
+      <>
+        <Helmet>
+          <title>{!!record ? record.title : getIdFromPathname(location.pathname)}</title>
+          {!!record && !!record.scopeContentNote && <meta name="description" content={record.scopeContentNote} />}
+          <meta name="" content="" />
+          <meta name="twitter:title" content={!!record ? record.title : getIdFromPathname(location.pathname)} />
+          <meta name="twitter:site" content="@FIXME" />
+          {!!record && !!record.scopeContentNote && <meta name="twitter:card" content={record.scopeContentNote} />}
+          {!!record && !!record.scopeContentNote && (
+            <meta name="twitter:description" content={record.scopeContentNote} />
+          )}
+          {!!record && <meta name="twitter:image" content={iiifImage(record, '1080')} />}
+          <meta property="og:title" content={!!record ? record.title : getIdFromPathname(location.pathname)} />
+          {!!record && !!record.scopeContentNote && <meta name="og:description" content={record.scopeContentNote} />}
+          <meta property="og:site_name" content="FIXME" />
+          <meta property="og:url" content={window.location} />
+          <meta property="og:type" content="article" />
+          {!!record && <meta property="og:image" content={iiifImage(record, '1080')} />}
+        </Helmet>
+        <Layout.Padding style={{ marginTop: '2rem', marginBottom: '3rem' }}>
+          <Layout.Wrapper>
+            <Shave textRef={titleTextRef} maxHeight={130} options={{ character: '&nbsp; ' }}>
+              <Text.H1 ref={titleTextRef}>{record.title}</Text.H1>
+            </Shave>
+            <MetaWrapper>
+              {tribalNation && (
+                <MetaStyled
+                  label="Tribal Nation"
+                  outine
+                  items={[
+                    {
+                      label: record.tribes,
+                      link: `/tribal-nations/${tribalNation ? tribalNation.slug : ''}`,
+                      onClick: () => {
+                        history.push(`/tribal-nations/${tribalNation ? tribalNation.slug : ''}`);
                       },
-                    ]}
-                  ></MetaStyled>
-                )}
-                {!!record.date && <MetaStyled label="Date" outine items={[{ label: record.date }]}></MetaStyled>}
-                {!!recordTopics.length && (
-                  <MetaStyled
-                    label="Topics"
-                    outine
-                    items={recordTopics.map(topic => {
-                      return {
-                        label: topic.name,
-                        link: `/topics/${topic.slug}`,
-                        onClick: () => {
-                          history.push(`/topics/${topic.slug}`);
-                        },
-                      };
-                    })}
-                  ></MetaStyled>
-                )}{' '}
-                {!!recordStates.length && (
-                  <MetaStyled
-                    label="States"
-                    outine
-                    items={recordStates.map(state => {
-                      return {
-                        label: state.name,
-                        link: `/states/${state.slug}`,
-                        onClick: () => {
-                          history.push(`/states/${state.slug}`);
-                        },
-                      };
-                    })}
-                  ></MetaStyled>
-                )}
-              </MetaWrapper>
-            </Layout.Wrapper>
-          </Layout.Padding>
-          <ImageViewer record={record} objects={objects} />
-          <Layout.Padding style={{ marginTop: '2rem', marginBottom: '3rem' }}>
-            <Layout.Wrapper>
-              <Tabs>
-                <TabList style={{ overflowX: 'hidden' }}>
-                  {' '}
-                  {/* prevents the scrollbar on Windows */}
-                  <Tab>
-                    <DetailsIcon width={20} />
-                    <Text.Label>Details</Text.Label>
-                  </Tab>
-                  <Tab>
-                    <CitationIcon width={20} />
-                    <Text.Label>Citation</Text.Label>
-                  </Tab>
-                </TabList>
+                    },
+                  ]}
+                ></MetaStyled>
+              )}
+              {!!record.date && <MetaStyled label="Date" outine items={[{ label: record.date }]}></MetaStyled>}
+              {!!recordTopics.length && (
+                <MetaStyled
+                  label="Topics"
+                  outine
+                  items={recordTopics.map(topic => {
+                    return {
+                      label: topic.name,
+                      link: `/topics/${topic.slug}`,
+                      onClick: () => {
+                        history.push(`/topics/${topic.slug}`);
+                      },
+                    };
+                  })}
+                ></MetaStyled>
+              )}{' '}
+              {!!recordStates.length && (
+                <MetaStyled
+                  label="States"
+                  outine
+                  items={recordStates.map(state => {
+                    return {
+                      label: state.name,
+                      link: `/states/${state.slug}`,
+                      onClick: () => {
+                        history.push(`/states/${state.slug}`);
+                      },
+                    };
+                  })}
+                ></MetaStyled>
+              )}
+            </MetaWrapper>
+          </Layout.Wrapper>
+        </Layout.Padding>
+        <ImageViewer record={record} objects={objects} />
+        <Layout.Padding style={{ marginTop: '2rem', marginBottom: '3rem' }}>
+          <Layout.Wrapper>
+            <Tabs>
+              <TabList style={{ overflowX: 'hidden' }}>
+                {' '}
+                {/* prevents the scrollbar on Windows */}
+                <Tab>
+                  <DetailsIcon width={20} />
+                  <Text.Label>Details</Text.Label>
+                </Tab>
+                <Tab>
+                  <CitationIcon width={20} />
+                  <Text.Label>Citation</Text.Label>
+                </Tab>
+              </TabList>
 
-                <TabPanel>
-                  {!!record.scopeContentNote && (
-                    <Table.RowStyles>
-                      <Table.LabelStyles>
-                        <Text.Label style={{ fontSize: '13px', fontWeight: 'normal' }}>Description:</Text.Label>
-                      </Table.LabelStyles>
-                      <Table.ValueStyles>
-                        <p>{record.scopeContentNote}</p>
-                      </Table.ValueStyles>
-                    </Table.RowStyles>
-                  )}
+              <TabPanel>
+                {!!record.scopeContentNote && (
                   <Table.RowStyles>
                     <Table.LabelStyles>
-                      <Text.Label style={{ fontSize: '13px', fontWeight: 'normal' }}>
-                        National Archives Catalog Identifier:
-                      </Text.Label>
+                      <Text.Label style={{ fontSize: '13px', fontWeight: 'normal' }}>Description:</Text.Label>
                     </Table.LabelStyles>
                     <Table.ValueStyles>
-                      <ExternalLink href={`${externalUrls.catalogRecordDetail}/${record.naId}`}>
-                        {record.naId}
-                      </ExternalLink>
+                      <p>{record.scopeContentNote}</p>
                     </Table.ValueStyles>
                   </Table.RowStyles>
-                </TabPanel>
-                <TabPanel>
-                  <Table.RowStyles>
-                    <Text.Rich>
-                      <p>
-                        {record.title},{!!record.date ? `${record.date} ` : ' '}[Photographs and other Graphic
-                        Materials]; Records of the Bureau of Indian Affairs, Record Group 75;
-                        {!!record.location ? ` ${record.location}` : ' '}[online version available through the National
-                        Archives Catalog (National Archives Identifier {record.naId}) at{' '}
-                        <a href={`https://catalog.archives.gov/id/${record.naId}`}>
-                          https://catalog.archives.gov/id/{record.naId}
-                        </a>
-                        ;{' '}
-                        {new Intl.DateTimeFormat('en-US', {
-                          month: 'long',
-                          day: 'numeric',
-                          year: 'numeric',
-                        }).format(new Date())}
-                        ].
-                      </p>
-                    </Text.Rich>
-                  </Table.RowStyles>
-                </TabPanel>
-              </Tabs>
+                )}
+                <Table.RowStyles>
+                  <Table.LabelStyles>
+                    <Text.Label style={{ fontSize: '13px', fontWeight: 'normal' }}>
+                      National Archives Catalog Identifier:
+                    </Text.Label>
+                  </Table.LabelStyles>
+                  <Table.ValueStyles>
+                    <ExternalLink href={`${externalUrls.catalogRecordDetail}/${record.naId}`}>
+                      {record.naId}
+                    </ExternalLink>
+                  </Table.ValueStyles>
+                </Table.RowStyles>
+              </TabPanel>
+              <TabPanel>
+                <Table.RowStyles>
+                  <Text.Rich>
+                    <p>
+                      {record.title},{!!record.date ? `${record.date} ` : ' '}[Photographs and other Graphic Materials];
+                      Records of the Bureau of Indian Affairs, Record Group 75;
+                      {!!record.location ? ` ${record.location}` : ' '}[online version available through the National
+                      Archives Catalog (National Archives Identifier {record.naId}) at{' '}
+                      <a href={`https://catalog.archives.gov/id/${record.naId}`}>
+                        https://catalog.archives.gov/id/{record.naId}
+                      </a>
+                      ;{' '}
+                      {new Intl.DateTimeFormat('en-US', {
+                        month: 'long',
+                        day: 'numeric',
+                        year: 'numeric',
+                      }).format(new Date())}
+                      ].
+                    </p>
+                  </Text.Rich>
+                </Table.RowStyles>
+              </TabPanel>
+            </Tabs>
+          </Layout.Wrapper>
+        </Layout.Padding>
+        {!!thumbnailResults.length && (
+          <Layout.Padding style={{ marginBottom: '3rem' }}>
+            <Layout.Wrapper>
+              <SectionHeader>
+                <SeriesIcon width={20}></SeriesIcon>
+                <span>Also in this series</span>
+              </SectionHeader>
+              <SeriesHeader>
+                <p style={{ marginRight: '20px' }}>
+                  There are {thumbnailResults.length.toLocaleString('en')} other digitized records in the archival
+                  series {record.parentSeriesTitle}
+                </p>
+
+                <ExternalLink
+                  style={{ flex: '1 0 auto' }}
+                  href={`https://catalog.archives.gov/search?q=*:*&f.ancestorNaIds=${record.parentSeriesNaId}&sort=naIdSort%20asc&f.materialsType=photographsandgraphics`}
+                >
+                  View All in the Catalog
+                </ExternalLink>
+              </SeriesHeader>
+              <Results singleRow data={sampleSize(thumbnailResults, 3)} fidelity={250} />
             </Layout.Wrapper>
           </Layout.Padding>
-          {!!thumbnailResults.length && (
-            <Layout.Padding style={{ marginBottom: '3rem' }}>
-              <Layout.Wrapper>
-                <SectionHeader>
-                  <SeriesIcon width={20}></SeriesIcon>
-                  <span>Also in this series</span>
-                </SectionHeader>
-                <SeriesHeader>
-                  <p style={{ marginRight: '20px' }}>
-                    There are {thumbnailResults.length.toLocaleString('en')} other digitized records in the archival
-                    series {record.parentSeriesTitle}
-                  </p>
-
-                  <ExternalLink
-                    style={{ flex: '1 0 auto' }}
-                    href={`https://catalog.archives.gov/search?q=*:*&f.ancestorNaIds=${record.parentSeriesNaId}&sort=naIdSort%20asc&f.materialsType=photographsandgraphics`}
-                  >
-                    View All in the Catalog
-                  </ExternalLink>
-                </SeriesHeader>
-                <Results singleRow data={sampleSize(thumbnailResults, 3)} fidelity={250} />
-              </Layout.Wrapper>
-            </Layout.Padding>
-          )}
-        </>
-      )}
-      {!record && finishedQuery && <Redirect to="/404"></Redirect>}
-    </>
-  );
+        )}
+      </>
+    );
+  } else if (!record && finishedQuery) {
+    return <Redirect to="/404"></Redirect>;
+  } else {
+    return <PageLoader />;
+  }
 };
 
 export default Record;
